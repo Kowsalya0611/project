9a9578e6f748e31849bc6fc0ee5695ca9669b341
Kowsalya0611 <148540351+Kowsalya0611@users.noreply.github.com>
2024-03-17 12:27:30 +0530

    Update homepage.js

--- homepage.js
+++ homepage.js
@@ -2,7 +2,7 @@ let ooty = document.querySelector(".ooty");
 let kotagiri = document.querySelector(".kotagiri");
 let coonoor = document.querySelector(".coonoor");
 ooty.onclick = function () {
-  let url1 = "https://rawcdn.githack.com/Kowsalya0611/project/10b821f06e44908293605b253bfad5090e4b8b51/ooty.html";
+  let url1 = "https://github.com/Kowsalya0611/project/blob/main/ooty.html";
   window.open(url1, "_self");
 };
 kotagiri.onclick = function () {
