599f7d9d02c1de6ed2fa789e236cf82457ef117f
Kowsalya0611 <kowsalyaeswaran04@gmail.com>
2024-03-17 12:39:58 +0530

    change path

--- homepage.js
+++ homepage.js
@@ -2,7 +2,7 @@ let ooty = document.querySelector(".ooty");
 let kotagiri = document.querySelector(".kotagiri");
 let coonoor = document.querySelector(".coonoor");
 ooty.onclick = function () {
-  let url1 = "https://rawcdn.githack.com/Kowsalya0611/project/9a9578e6f748e31849bc6fc0ee5695ca9669b341/ooty.html";
+  let url1 = "http://127.0.0.1:5500/ooty.html";
   window.open(url1, "_self");
 };
 kotagiri.onclick = function () {
