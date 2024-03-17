d7f60cf7e8564763e34eaab41175260c11b528ad
Kowsalya0611 <148540351+Kowsalya0611@users.noreply.github.com>
2024-03-17 12:25:37 +0530

    changing path online

--- homepage.js
+++ homepage.js
@@ -2,7 +2,7 @@ let ooty = document.querySelector(".ooty");
 let kotagiri = document.querySelector(".kotagiri");
 let coonoor = document.querySelector(".coonoor");
 ooty.onclick = function () {
-  let url1 = "file:///D:/Main/project/ooty.html";
+  let url1 = "https://rawcdn.githack.com/Kowsalya0611/project/10b821f06e44908293605b253bfad5090e4b8b51/ooty.html";
   window.open(url1, "_self");
 };
 kotagiri.onclick = function () {
