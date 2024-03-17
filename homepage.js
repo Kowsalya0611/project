412b1802b4ef86ba99b06dd452104688a0278dcc
Kowsalya0611 <kowsalyaeswaran04@gmail.com>
2024-03-17 12:11:39 +0530

    changing path

--- homepage.js
+++ homepage.js
@@ -2,7 +2,7 @@ let ooty = document.querySelector(".ooty");
 let kotagiri = document.querySelector(".kotagiri");
 let coonoor = document.querySelector(".coonoor");
 ooty.onclick = function () {
-  let url1 = "http://127.0.0.1:5500/ooty.html";
+  let url1 = "file:///D:/Main/project/ooty.html";
   window.open(url1, "_self");
 };
 kotagiri.onclick = function () {
